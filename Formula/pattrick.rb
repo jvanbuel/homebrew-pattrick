5a7a75fa0f76b99c4d335b4abc05b8f738baf83d
Jan Vanbuel <janvanbuel@hotmail.com>
2023-01-02 09:40:45 +0100

    Update pattrick.rb

--- Formula/pattrick.rb
+++ Formula/pattrick.rb
@@ -4,8 +4,8 @@
 class Pattrick < Formula
   desc "CLI for managing Personal Access Tokens (PAT) in Azure DevOps"
   homepage "https://github.com/jvanbuel/pattrick"
-  url "https://github.com/jvanbuel/pattrick/releases/download/v0.3.0/pattrick-x86_64-apple-darwin.tar.gz"
-  sha256 "7590926e6be5635f7d69b7e79b8c17650ec862c8a2407759422329af05a721bd"
+  url "https://github.com/jvanbuel/pattrick/releases/download/v0.3.3/pattrick-x86_64-apple-darwin.tar.gz"
+  sha256 "5203152973240bf073000f80499d1d3a1c8fe82b0d8b9979673073f1b1412a39"
   version "0.3.0"
 
   def install
